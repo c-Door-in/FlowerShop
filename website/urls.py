cfed29a6bbea54691294fa9fe761cde15cacb256
c_Door in <egor1@mail.ru>
2023-01-18 00:56:41 +0300

    Сделал шаблоны всех страниц

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,14 @@ from . import views
 
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('consultation/', views.consultation, name='consultation'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('card/', views.card, name='card'),
+    path('order/', views.order, name='order'),
+    path('order-step/', views.order_step, name='order_step'),
+    path('quiz/', views.quiz, name='quiz'),
+    path('quiz-step/', views.quiz_step, name='quiz_step'),
+    path('result/', views.result, name='result'),
 ]
 
